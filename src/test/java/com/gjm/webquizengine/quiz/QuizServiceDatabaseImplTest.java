db0519ee339f7017cc5093c5696011a9e47b1b79
Grzegorz-Malyska <vaversky.official@gmail.com>
2020-07-23 09:48:12 +0200

    Refactored QuizService tests.

--- src/test/java/com/gjm/webquizengine/quiz/QuizServiceDatabaseImplTest.java
+++ src/test/java/com/gjm/webquizengine/quiz/QuizServiceDatabaseImplTest.java
@@ -1,20 +1,21 @@
 package com.gjm.webquizengine.quiz;
 
+import com.gjm.webquizengine.quiz.error_handling.NoQuizException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Spy;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Collections;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class QuizServiceDatabaseImplTest {
-    @Spy
+    @Mock
     private QuizRepository quizRepository;
 
     private QuizServiceDatabaseImpl quizService;
@@ -22,7 +23,8 @@ class QuizServiceDatabaseImplTest {
 
     @BeforeEach
     void setUp() {
-        quizzes = List.of(new Quiz("title1", "text1", Collections.emptyList(), null), new Quiz("title2", "text2", Collections.emptyList(), null));
+        quizzes = List.of(new Quiz("title1", "text1", Collections.emptyList(), List.of(0, 1)),
+                          new Quiz("title2", "text2", Collections.emptyList(), null));
         quizzes.get(0).setId(5);
 
         quizService = new QuizServiceDatabaseImpl(quizRepository);
@@ -48,5 +50,20 @@ class QuizServiceDatabaseImplTest {
         quizService.deleteQuiz(5);
 
         verify(quizRepository, times(1)).deleteById(5);
+    }
+
+    @Test
+    void findQuizById() {
+        when(quizRepository.findAll()).thenReturn(quizzes);
+
+        assertEquals(quizzes.get(0), quizService.findQuizById(5));
+        assertThrows(NoQuizException.class, () -> quizService.findQuizById(1));
+    }
+
+    @Test
+    void solveQuiz() {
+        when(quizRepository.findAll()).thenReturn(quizzes);
+
+        assertTrue(quizService.solveQuiz(5, List.of(0, 1)));
     }
 }
--- src/test/java/com/gjm/webquizengine/quiz/QuizServiceInMemoryImplTest.java
+++ src/test/java/com/gjm/webquizengine/quiz/QuizServiceInMemoryImplTest.java
@@ -1,9 +1,11 @@
 package com.gjm.webquizengine.quiz;
 
+import com.gjm.webquizengine.quiz.error_handling.NoQuizException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Collections;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,27 +17,36 @@ class QuizServiceInMemoryImplTest {
     void setUp() {
         quizService = new QuizServiceInMemoryImpl();
 
-        quiz = new Quiz("title", "text", Collections.emptyList(), Collections.emptyList());
+        quiz = new Quiz("title", "text", Collections.emptyList(), List.of(0, 1));
         quiz.setId(5);
+        quizService.addQuiz(quiz);
     }
 
     @Test
     void findAllQuizzes() {
-        assertEquals(Collections.emptyList(), quizService.findAllQuizzes());
+        assertEquals(List.of(quiz), quizService.findAllQuizzes());
     }
 
     @Test
     void addQuiz() {
-        quizService.addQuiz(quiz);
-
         assertTrue(quizService.findAllQuizzes().contains(quiz));
     }
 
     @Test
     void deleteQuiz() {
-        quizService.addQuiz(quiz);
         quizService.deleteQuiz(5);
 
         assertFalse(quizService.findAllQuizzes().contains(quiz));
+    }
+
+    @Test
+    void findQuizById() {
+        assertEquals(quiz, quizService.findQuizById(5));
+        assertThrows(NoQuizException.class, () -> quizService.findQuizById(1));
+    }
+
+    @Test
+    void solveQuiz() {
+        assertTrue(quizService.solveQuiz(5, List.of(0, 1)));
     }
 }
