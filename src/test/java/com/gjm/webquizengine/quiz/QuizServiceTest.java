079d235860d8ce387babe73fa9305580ee302efa
Grzegorz-Malyska <vaversky.official@gmail.com>
2020-07-23 09:14:20 +0200

    CircleCI bug fixing

--- src/test/java/com/gjm/webquizengine/quiz/QuizServiceTest.java
+++ src/test/java/com/gjm/webquizengine/quiz/QuizServiceTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 
 import java.util.List;
 
@@ -13,6 +15,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 class QuizServiceTest {
     @Spy
     private QuizService quizService;
@@ -27,19 +30,15 @@ class QuizServiceTest {
 
     @Test
     void findQuizById() {
-        try {
-            when(quizService.findAllQuizzes()).thenReturn(List.of(quiz));
+        when(quizService.findAllQuizzes()).thenReturn(List.of(quiz));
 
-            assertEquals(quiz, quizService.findQuizById(5));
-            assertThrows(NoQuizException.class, () -> quizService.findQuizById(1));
-        } catch(Exception exception) {
-            System.out.println(exception.getMessage());
-        }
+        assertEquals(quiz, quizService.findQuizById(5));
+        assertThrows(NoQuizException.class, () -> quizService.findQuizById(1));
     }
 
     @Test
     void solveQuiz() {
-        when(quizService.findAllQuizzes()).thenReturn(List.of(quiz));
+        when(quizService.findQuizById(5)).thenReturn(quiz);
 
         assertTrue(quizService.solveQuiz(5, List.of(0, 1)));
     }
